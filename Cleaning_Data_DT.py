c3a838b59858d9aa9d6329bb4178b5bfc579c9f0
kristinslevine <54111813+kristinslevine@users.noreply.github.com>
2020-04-09 14:35:02 -0400

    More changes

--- Cleaning_Data_DT.py
+++ Cleaning_Data_DT.py
@@ -1,6 +1,26 @@
+#m_ascending = m.sort_values(by = 'vac_rate')
+#print(m_ascending.head(5))
+#print(m_ascending.tail(5))
+
+#want to experiement with exemption rates too
+#print("Total Exemption Rates")
+#m['total_exempt'] = m['xrel'] + m['xmed'] + m['xper']
+
 import pandas as pd
-import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+import sys
+import numpy as np
+import pandas as pd
+
 m = pd.read_csv('all-measles-rates.csv')
 
 import pandas as pd
@@ -16,30 +36,30 @@ print('\n')
 print("Eliminate schools with no enrollment data")
 m = m.loc[(m['enroll'] > 0)]
 #what do we do with a school of enrollment 1? Is that valid data??
-print('\n')
 
 print('Combine mmr and overall vaccination rates: use mmr if given, overall if no mmr available.')
 m['vac_rate'] = m['mmr']
 m['vac_rate'] = m['mmr'].where(m['mmr'] > 0, m['overall'] )
-print('\n')
+
 
 print('Eliminate schools with a negative vac_rate')
 m = m.loc[(m['vac_rate'] >= 0)]
+
+print("Is vaccination rate at least 95%? ")
+m['at_least_95'] = (m['vac_rate'] >= 95)
 print('\n')
+
+print('Find the mean vac_rate per state:')
+
 
 print(m.head(5))
 print(m.tail(5))
 
-m_tree = m[['name', 'state', 'city', 'county', 'type', 'enroll', 'vac_rate']]
+m_tree = m[['state', 'city', 'county', 'type', 'enroll', 'at_least_95']]
 
 print(m_tree.head(5))
+print('\n')
 print(m_tree.tail(5))
+print('\n')
 
 
-#m_ascending = m.sort_values(by = 'vac_rate')
-#print(m_ascending.head(5))
-#print(m_ascending.tail(5))
-
-#want to experiement with exemption rates too
-#print("Total Exemption Rates")
-#m['total_exempt'] = m['xrel'] + m['xmed'] + m['xper']
