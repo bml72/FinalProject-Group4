6c64b1ed0476d395acaa6b11694ced8d45b25584
kristinslevine <54111813+kristinslevine@users.noreply.github.com>
2020-04-20 10:18:25 -0400

    Updates

--- Python_Files/Decision_Tree_Over_95.py
+++ Python_Files/Decision_Tree_Over_95.py
@@ -138,24 +138,24 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X,Y)
 
 # Visualize data
-dot_data = tree.export_graphviz(clf,
-                                feature_names=data_feature_names,
-                                out_file=None,
-                                filled=True,
-                                rounded=True)
-graph = pydotplus.graph_from_dot_data(dot_data)
+#dot_data = tree.export_graphviz(clf,
+#                                feature_names=data_feature_names,
+#                                out_file=None,
+#                                filled=True,
+#                                rounded=True)
+#graph = pydotplus.graph_from_dot_data(dot_data)
 
-colors = ('turquoise', 'orange')
-edges = collections.defaultdict(list)
+#colors = ('turquoise', 'orange')
+#edges = collections.defaultdict(list)
 
-for edge in graph.get_edge_list():
-    edges[edge.get_source()].append(int(edge.get_destination()))
+#for edge in graph.get_edge_list():
+#    edges[edge.get_source()].append(int(edge.get_destination()))
 
-for edge in edges:
-    edges[edge].sort()
-    for i in range(2):
-        dest = graph.get_node(str(edges[edge][i]))[0]
-        dest.set_fillcolor(colors[i])
+#for edge in edges:
+#    edges[edge].sort()
+#    for i in range(2):
+#        dest = graph.get_node(str(edges[edge][i]))[0]
+#        dest.set_fillcolor(colors[i])
 
 #graph.write_png('m_tree.png')
 #graph.write_svg('m_tree.svg')
