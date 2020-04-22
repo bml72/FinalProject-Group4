95571af121325768e7fadc991d990aed4b84230e
Russell Moncrief <russell_m2000@yahoo.com>
2020-04-21 22:20:38 -0400

    added regression model
    second commit

--- russell_final.py
+++ russell_final.py
@@ -1,81 +1,129 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+#from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+from sklearn import preprocessing
 
-#Read data
-data = pd.read_csv('all-measles-rates.csv', sep=",", error_bad_lines=False)
-data.columns = ['index', 'state', 'year', 'name', 'type', 'city', 'county', 'district', 'enroll', 'mmr', 'overall', 'xrel', 'xmed', 'xper']
-print('Description of Data')
-print(data.describe())
-print()
+def regression(state):
+	data = pd.read_csv('all-measles-rates.csv', sep=",", error_bad_lines=False)
+	data.columns = ['index', 'state', 'year', 'name', 'type', 'city', 'county', 
+	'district', 'enroll', 'mmr', 'overall', 'xrel', 'xmed', 'xper']
 
-#Check missing values
-print('Missing Values:')
-print(data.isnull().sum())
-print()
+	print('Description of Data')
+	print(data.describe())
+	print()
 
-#Drop missing values for year
-#print('Dropped Missing years')
-#data = data[data['year'].notna()]
-#print(data.isnull().sum())
-#print()
+	print('Description of Categorical Data')
+	print(data.describe(include = ['O']))
+	print()
 
-#Fill missing values with means
-enroll_mean = data['enroll'].mean(axis=0)
-data['enroll'].fillna(enroll_mean, inplace=True)
-xrel_mean = data['xrel'].mean(axis=0)
-data['xrel'].fillna(xrel_mean, inplace=True)
-xmed_mean = data['xmed'].mean(axis=0)
-data['xmed'].fillna(xmed_mean, inplace=True)
-xper_mean = data['xper'].mean(axis=0)
-data['xper'].fillna(xper_mean, inplace=True)
-print('Missing Values:')
-print(data.isnull().sum())
-print()
+	#Check missing values
+	print('Missing Values:')
+	print(data.isnull().sum())
+	print()
 
-#Drop remaining missing values
-#data = data[data['type'].notna()]
-#data = data[data['city'].notna()]
-#data = data[data['county'].notna()]
-#data = data.drop('district', 1)
-#print('Cleaned Dataset')
-#print(data.isnull().sum())
-#print()
-#print('Cleaned Data Description')
-#print(data.describe())
-#print(data)
-#print()
+	#Impute means into missing values
+	enroll_mean = data['enroll'].mean(axis=0)
+	data['enroll'].fillna(enroll_mean, inplace=True)
+	xrel_mean = data['xrel'].mean(axis=0)
+	data['xrel'].fillna(xrel_mean, inplace=True)
+	xmed_mean = data['xmed'].mean(axis=0)
+	data['xmed'].fillna(xmed_mean, inplace=True)
+	xper_mean = data['xper'].mean(axis=0)
+	data['xper'].fillna(xper_mean, inplace=True)
+	print('Missing Values:')
+	print(data.isnull().sum())
+	print()
 
-#Graphs
-colors = ['red', 'tan', 'lime']
-plt.hist(data['enroll'], bins=100, color='green')
-plt.title('Distribution of Enrollment')
-plt.show()
-plt.hist(data['mmr'], bins=10, color='yellow')
-plt.title('Distribution of MMR Rate')
-plt.show()
-plt.hist(data['overall'], bins=10, color='blue')
-plt.title('Distribution of Overall Rate')
-plt.show()
-plt.hist(data['xrel'], bins=10, color='red')
-plt.title('Distribution of Xrel Rate')
-plt.show()
-plt.hist(data['xmed'], bins=10, color='orange')
-plt.title('Distribution of Xmed Rate')
-plt.show()
-plt.hist(data['xper'], bins=10, color='lime')
-plt.title('Distribution of Xper Rate')
-plt.show()
+	#Subset data by state
+	data = data[data["state"] == state]
 
-sns.boxplot(x = "state", y = "mmr" , data = data, palette="PRGn")
-plt.xticks(rotation=90)
-plt.show()
+	#Scatter plots of variables of interest
+	plt.scatter(data['enroll'], data['mmr'])
+	plt.title('MMR Vs Enroll')
+	plt.xlabel('Enrollment')
+	plt.ylabel('MMR Rate')
+	plt.show()
 
-sns.boxplot(x = "type", y = "mmr" , data = data, palette="PRGn")
-plt.xticks(rotation=90)
-plt.show()
+	plt.scatter(data['overall'], data['mmr'])
+	plt.title('MMR Vs Overall')
+	plt.xlabel('Overall Rate')
+	plt.ylabel('MMR Rate')
+	plt.show()
 
+	plt.scatter(data['xrel'], data['mmr'])
+	plt.title('MMR Vs Xrel')
+	plt.xlabel('Religious Exemptions')
+	plt.ylabel('MMR Rate')
+	plt.show()
+
+	plt.scatter(data['xmed'], data['mmr'])
+	plt.title('MMR Vs Xmed')
+	plt.xlabel('Medical Exemptions')
+	plt.ylabel('MMR Rate')
+	plt.show()
+
+	plt.scatter(data['xper'], data['mmr'])
+	plt.title('MMR Vs Xper')
+	plt.xlabel('Personal Exemptions')
+	plt.ylabel('MMR Rate')
+	plt.show()
+
+	#Define feature and target data
+	data.loc[data.mmr == -1, 'mmr'] = 0
+	data.loc[data.overall == -1, 'overall'] = 0
+	y = data['mmr']
+	x = data[['enroll', 'overall', 'xrel', 'xmed', 'xper']]
+	
+
+	#Test train split and linear regression
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=0)
+	mlr = LinearRegression()
+	mlr.fit(x_train, y_train)
+	y_pred = mlr.predict(x_test)
+
+	#Print coefficients and metrics
+	coeff_df = pd.DataFrame(mlr.coef_, x.columns, columns=['Coefficient'])
+	coeff_df
+	print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+	print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+	print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+	#View actual vs predicted and model score
+	df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+	print(df)
+	print('The score of the model: ', round(mlr.score(x_test, y_test), 3))
+	print()
+	
+	
+	#Normalize data 
+	min_max_scaler = preprocessing.MinMaxScaler()
+	x_scaled = min_max_scaler.fit_transform(x)
+	x = pd.DataFrame(x_scaled)
+	y = data['mmr'].values
+	y_scaled = min_max_scaler.fit_transform(y.reshape(-1, 1)).reshape(-1)
+	y = pd.DataFrame(y_scaled)
+	
+	#Test train split and linear regression
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.20, random_state=0)
+	mlr = LinearRegression()
+	mlr.fit(x_train, y_train)
+	y_pred = mlr.predict(x_test)
+	
+	#Print metrics 
+	print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+	print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+	print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+	print('The score of the model: ', round(mlr.score(x_test, y_test), 3))
+
+
+	
+
+
+if __name__=="__main__":regression("Oregon")
 
 
 
