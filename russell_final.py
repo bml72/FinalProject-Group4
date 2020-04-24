94159d522da1b0943afb414ecb1d6892289a1dc6
Russell Moncrief <russell_m2000@yahoo.com>
2020-04-23 20:48:57 -0400

    code

--- russell_final.py
+++ russell_final.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
 from sklearn import preprocessing
 
